67f8eb25ded65f1a3e20af9ad2212e0a37cdac2f
Cristian-Alexandru Ruja <christianajur@gmail.com>
2022-08-21 12:49:12 +0300

    First Update

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Giphy from "./components/Giphy";
+
+const App = () => {
+  return <div>
+    <Giphy></Giphy>
+  </div>;
+};
+
+export default App;
---
+++ src/components/Giphy.jsx
@@ -0,0 +1,16 @@
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+
+function Giphy() {
+
+    useEffect(()=> {
+        const fetchData = async () => {
+
+        }
+    })
+  return (
+    <div>Giphy</div>
+  )
+}
+
+export default Giphy
