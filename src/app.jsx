8c4b80ecebf874525f34f85b2587dcb27e234157
Cristian-Alexandru Ruja <christianajur@gmail.com>
2022-08-24 15:26:14 +0300

    Update: tasks done

--- src/app.jsx
+++ src/app.jsx
@@ -3,28 +3,11 @@ import Giphy from "./Components/Giphy";
 import "./Styles/App.css";
 import "./Styles/Form.css";
 import "./Styles/Loader.css";
-import { db } from "./firebase-config";
-import { collection, getDocs } from "firebase/firestore";
 
 const App = () => {
-  const [history, setHistory] = useState([]);
-  const historyCollectionRef = collection(db, "history");
-  useEffect(() => {
-    const getHistory = async () => {
-      const data = await getDocs(historyCollectionRef);
-      setHistory(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
-
-    getHistory();
-  }, []);
 
   return (
     <div>
-      {/* {history.map((el) => {
-        return <div>
-          <h1>Searched: {el.searchHistory}</h1>
-        </div>;
-      })} */}
       <Giphy/>
     </div>
   );
--- src/components/Giphy.jsx
+++ src/components/Giphy.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState, Fragment } from "react";
 import axios from "axios";
 import Loader from "./Loader";
+import { db } from "./firebase-config";
+import { collection, getDocs, addDoc } from "firebase/firestore";
 
 function Giphy() {
   const [data, setData] = useState([]);
   const [search, setSearch] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [id, setId] = useState(0);
+  // const [history, setHistory] = useState([]);
+  const historyCollectionRef = collection(db, "history");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -51,16 +55,27 @@ function Giphy() {
         q: search,
       },
     });
+    const createSearchHistory = async () => {
+      await addDoc(historyCollectionRef, { searchHistory: search });
+    };
+    createSearchHistory();
     setId(id + 1);
     setData(results.data.data);
     setIsLoading(false);
-    
   };
 
-
-  useEffect(()=> {
+  useEffect(() => {
     window.localStorage.setItem(`search ID ${id}`, JSON.stringify(search));
-  },[search])
+  }, [search]);
+
+  // useEffect(() => {
+  //   const getHistory = async () => {
+  //     const data = await getDocs(historyCollectionRef);
+  //     setHistory(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  //   };
+
+  //   getHistory();
+  // }, []);
 
   return (
     <Fragment>
@@ -80,6 +95,11 @@ function Giphy() {
         10 GIF's available for: <b>{search}</b>
       </p>
       <div className="container gifs">{renderGifs()}</div>
+            {/* {history.map((el) => {
+        return <div>
+          <h1>Searched: {el.searchHistory}</h1>
+        </div>;
+      })} */}
     </Fragment>
   );
 }
